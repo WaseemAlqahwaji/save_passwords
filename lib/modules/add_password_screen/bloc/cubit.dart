02fba1e24d84e60f596914ab1c830cffeeb996d9
Waseem_Alqahwaji <waseemalqahwaji123@gmail.com>
2022-07-29 05:20:21 -0500

    Save Password app

--- lib/modules/add_password_screen/bloc/cubit.dart
+++ lib/modules/add_password_screen/bloc/cubit.dart
@@ -5,25 +5,23 @@ import '../../../local_db/SQflite.dart';
 import '../../passwords_screen/lists_of_passwords/passwords_lists.dart';
 
 class AddPasswordCubit extends Cubit<AddPasswordStates> {
-  AddPasswordCubit(AddPasswordStates AddLayoutInitialState) : super(AddLayoutInitialState);
+  AddPasswordCubit(AddPasswordStates AddLayoutInitialState)
+      : super(AddLayoutInitialState);
 
   static AddPasswordCubit get(context) => BlocProvider.of(context);
   String? valueOfDropdownButton;
   MySql dbObj = MySql();
   String lableEmail = "Email :";
+  bool isEmail = true;
 
-  void onChangedDropDownButton(String value)
-  {
+  void onChangedDropDownButton(String value) {
     valueOfDropdownButton = value;
     emit(AddPasswordChangeDropdownButtonState());
-    if(value == "Note")
-    {
+    if (value == "Note") {
       lableEmail = "Note :";
       emit(AddPasswordChangeDropDownButtonSecureNote());
       print("note");
-    }
-    else if (value == "PayPal" || value == "Credit " || value == "Website" || value == "Media")
-    {
+    } else {
       lableEmail = "Email :";
       emit(AddPasswordChangeDropDownButtonSecureNote());
       print("email");
@@ -41,19 +39,24 @@ class AddPasswordCubit extends Cubit<AddPasswordStates> {
         .then((value) async {
       print("inserted done");
       if (category == "Media") {
-        PasswordsLists.mediaPasswords = await dbObj.getData("SELECT * FROM passwords WHERE category = 'Media'");
+        PasswordsLists.mediaPasswords = await dbObj
+            .getData("SELECT * FROM passwords WHERE category = 'Media'");
         print(PasswordsLists.mediaPasswords);
       } else if (category == "Website") {
-        PasswordsLists.websitePasswords = await dbObj.getData("SELECT * FROM passwords WHERE category = 'Website'");
+        PasswordsLists.websitePasswords = await dbObj
+            .getData("SELECT * FROM passwords WHERE category = 'Website'");
         print(PasswordsLists.websitePasswords);
       } else if (category == "Note") {
-        PasswordsLists.secureNote = await dbObj.getData("SELECT * FROM passwords WHERE category = 'Note'");
+        PasswordsLists.secureNote = await dbObj
+            .getData("SELECT * FROM passwords WHERE category = 'Note'");
         print(PasswordsLists.secureNote);
       } else if (category == "Credit") {
-        PasswordsLists.creditCardPasswords = await dbObj.getData("SELECT * FROM passwords WHERE category = 'Credit'");
+        PasswordsLists.creditCardPasswords = await dbObj
+            .getData("SELECT * FROM passwords WHERE category = 'Credit'");
         print(PasswordsLists.creditCardPasswords);
       } else if (category == "PayPal") {
-        PasswordsLists.paypalPasswords = await dbObj.getData("SELECT * FROM passwords WHERE category = 'PayPal'");
+        PasswordsLists.paypalPasswords = await dbObj
+            .getData("SELECT * FROM passwords WHERE category = 'PayPal'");
         print(PasswordsLists.paypalPasswords);
       }
     });
